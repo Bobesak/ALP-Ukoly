379c4531bcadb596b4c2d133203e80a3787feba8
Bobes <robert.nadymacek@gmail.com>
2024-09-18 10:06:56 +0200

    2nd ukol
    skibidi

--- Honsigovo tasks/Ukol 1 delka textu/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿string UserEntry;
-Console.WriteLine("write the desired text");
-UserEntry = Console.ReadLine();
-string trimmedtext;
-trimmedtext = UserEntry.Replace(" ", "");
-int txtlength = trimmedtext.Length;
-Console.WriteLine("Amount of characters is : " + txtlength);
-Console.ReadKey();
---
+++ Honsigovo tasks/Ukol 1 delka textu/Ukoly.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Ukoly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vyber si ukol: ");
+            Console.WriteLine("1. Delka textu");
+            Console.WriteLine("2. Soucet desetinnych cisel");
+            int UserChoice;
+            UserChoice = int.Parse(Console.ReadLine());
+            switch (UserChoice)
+            {
+                case 1:
+                    StringLength();
+                    break;
+                case 2:
+                    FloatSoucet();
+                    break;
+                default:
+                    Console.WriteLine("Zadal jsi spatnou volbu");
+                    break;
+            }
+        }
+        
+        static void StringLength()
+        {
+           string UserEntry;
+           Console.WriteLine("Napis text");
+           UserEntry = Console.ReadLine();
+           string trimmedtext;
+           trimmedtext = UserEntry.Replace(" ", "");
+           int txtlength = trimmedtext.Length;
+           Console.WriteLine("Pocet znaku je : " + txtlength);
+           Console.ReadKey(); 
+         }
+        
+        static void FloatSoucet()
+        {
+            float UserEntryFloat1;
+            float UserEntryFloat2;
+            Console.WriteLine("Napis prvni cislo");
+            UserEntryFloat1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Napis druhe cislo");
+            UserEntryFloat2 = float.Parse(Console.ReadLine());
+            float soucet = UserEntryFloat1 + UserEntryFloat2;
+            Console.WriteLine("Soucet dvou desetinnych cisel je: " + soucet);
+            Console.ReadKey();
+            
+        }
+    }
+}
+
+
+
+
